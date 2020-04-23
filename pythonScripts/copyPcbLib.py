8db96ccc4d8406e1c38bbbcd8cff215742a3d65d
XueYuSky <xueyusky@gmail.com>
2020-04-23 18:03:50 +0800

    优化复制库时的功能

--- pythonScripts/copyPcbLib.py
+++ pythonScripts/copyPcbLib.py
@@ -26,31 +26,24 @@ contentlen = len(content)
 
 print('文本总共有：' + str(contentlen) + '行。')
 
-def copyComponent(sourcefile,targetfileName):
-    b = targetFolder
-    #判断当前路径是否存在，没有则创建,
-    if not state:
-        os.makedirs(b)
-    # 如果已有该目录，……
+def copyComponent(source,targetFolder):
+    if not (os.path.exists(targetFolder)):
+        os.makedirs(targetFolder)
     else:   
         print('路径已存在！不再创建目录')
-    fullFileName = b + targetfileName +'\\' + '.PcbLib'
-    copyfile(sourcefile,fullFileName)
+    # 增加异常处理
+    try:
+        for line in content:
+            line= line.strip('\n').strip()
+            targetFileName = targetFolder +'\\' + line + '.PcbLib'
+            print(line)
+            copyfile(source,targetFileName)
+    except IOError as e:
+        print('IO文件错误不能复制文件： %s',e)
+        exit(1)
+    except:
+        print('出现错误，不能复制', sys.exc_info())
+        exit(1)
 
-# 增加异常处理
-try:
-    for line in content:
-        line= line.strip('\n').strip()
-        targetFileName = targetFolder +'\\' + line + '.PcbLib'
-        print(line)
-        # copyfile(source,targetFileName)
-        copyfile(source,targetFileName)
-        # copyComponent(source,line)
-except IOError as e:
-    print('IO文件错误不能复制文件： %s',e)
-    exit(1)
-except:
-    print('出现错误，不能复制', sys.exc_info())
-    exit(1)
-
+copyComponent(source,targetFolder)
 print('文件复制完成！')
